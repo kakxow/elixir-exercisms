cdc6bda9f441be6819b63ea8168ab51304f010f6
kakxow <kakxow@gmail.com>
2020-04-23 21:41:25 +0300

    protein translation

---
+++ protein-translation/lib/protein_translation.ex
@@ -0,0 +1,71 @@
+defmodule ProteinTranslation do
+  @map %{
+    "UGU" => "Cysteine",
+    "UGC" => "Cysteine",
+    "UUA" => "Leucine",
+    "UUG" => "Leucine",
+    "AUG" => "Methionine",
+    "UUU" => "Phenylalanine",
+    "UUC" => "Phenylalanine",
+    "UCU" => "Serine",
+    "UCC" => "Serine",
+    "UCA" => "Serine",
+    "UCG" => "Serine",
+    "UGG" => "Tryptophan",
+    "UAU" => "Tyrosine",
+    "UAC" => "Tyrosine",
+    "UAA" => "STOP",
+    "UAG" => "STOP",
+    "UGA" => "STOP"
+  }
+  @doc """
+  Given an RNA string, return a list of proteins specified by codons, in order.
+  """
+  @spec of_rna(String.t()) :: {atom, list(String.t())}
+  def of_rna(rna) do
+    [before_stop | _] = String.split(rna, ["UAA", "UAG", "UGA"])
+
+    try do
+      {:ok, parse_rna(before_stop)}
+    rescue
+      KeyError -> {:error, "invalid RNA"}
+    end
+  end
+
+  defp parse_rna(rna) when byte_size(rna) == 3 do
+    [Map.fetch!(@map, rna)]
+  end
+
+  defp parse_rna(<<codon::binary-size(3), rest::binary>>) do
+    [Map.fetch!(@map, codon) | parse_rna(rest)]
+  end
+
+  @doc """
+  Given a codon, return the corresponding protein
+
+  UGU -> Cysteine
+  UGC -> Cysteine
+  UUA -> Leucine
+  UUG -> Leucine
+  AUG -> Methionine
+  UUU -> Phenylalanine
+  UUC -> Phenylalanine
+  UCU -> Serine
+  UCC -> Serine
+  UCA -> Serine
+  UCG -> Serine
+  UGG -> Tryptophan
+  UAU -> Tyrosine
+  UAC -> Tyrosine
+  UAA -> STOP
+  UAG -> STOP
+  UGA -> STOP
+  """
+  @spec of_codon(String.t()) :: {atom, String.t()}
+  def of_codon(codon) do
+    case Map.fetch(@map, codon) do
+      {:ok, _} = value -> value
+      :error -> {:error, "invalid codon"}
+    end
+  end
+end
