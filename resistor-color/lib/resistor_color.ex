aa45783789cb1741fc1d0ba408f3c38d06f2c458
kakxow <kakxow@gmail.com>
2020-04-25 18:30:46 +0300

    resistor color

---
+++ resistor-color/lib/resistor_color.ex
@@ -0,0 +1,36 @@
+defmodule ResistorColor do
+  @moduledoc false
+  @color_to_code %{
+    "black" => 0,
+    "brown" => 1,
+    "red" => 2,
+    "orange" => 3,
+    "yellow" => 4,
+    "green" => 5,
+    "blue" => 6,
+    "violet" => 7,
+    "grey" => 8,
+    "white" => 9
+  }
+
+  @spec colors() :: list(String.t())
+  def colors do
+    [
+      "black",
+      "brown",
+      "red",
+      "orange",
+      "yellow",
+      "green",
+      "blue",
+      "violet",
+      "grey",
+      "white"
+    ]
+  end
+
+  @spec code(String.t()) :: integer()
+  def code(color) do
+    @color_to_code[color]
+  end
+end
