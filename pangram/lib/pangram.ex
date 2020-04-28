1d70be5db35b290fd0ff958bab927fb45defc8cf
kakxow <kakxow@gmail.com>
2020-04-28 14:27:38 +0300

    pangram

---
+++ pangram/lib/pangram.ex
@@ -0,0 +1,30 @@
+defmodule Pangram do
+  @doc """
+  Determines if a word or sentence is a pangram.
+  A pangram is a sentence using every letter of the alphabet at least once.
+
+  Returns a boolean.
+
+    ## Examples
+
+      iex> Pangram.pangram?("the quick brown fox jumps over the lazy dog")
+      true
+
+  """
+
+  @spec pangram?(String.t()) :: boolean
+  def pangram?(sentence) do
+    String.downcase(sentence)
+    |> to_charlist()
+    |> MapSet.new()
+    |> MapSet.intersection(MapSet.new(?a..?z))
+    |> decide
+  end
+
+  defp decide(set) do
+    case MapSet.size(set) do
+      26 -> true
+      _ -> false
+    end
+  end
+end
