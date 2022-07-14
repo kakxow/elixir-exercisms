0619fdca4ff334115d93d51b9e0620084e3221de
kakxow <kakxow@gmail.com>
2022-07-14 17:37:06 +0300

    Python script upadet with git commands

--- exel/exel/exel.py
+++ exel/exel/exel.py
@@ -1,3 +1,4 @@
+from re import sub
 import sys
 import subprocess
 import os
@@ -6,16 +7,15 @@ import json
 
 root_path = "/home/kakxow/snap/exercism/5/exercism/elixir"
 cli_arguments = sys.argv
-new_project_name = cli_arguments[3].split("=")[1]
 
 
-def execute_and_change_dir():
+def execute_and_change_dir(new_project_name):
     subprocess.run(cli_arguments[1:])
     path_to_new_project = f"{root_path}/{new_project_name}"
     os.chdir(path_to_new_project)
 
 
-def change_project_dir():
+def change_project_dir(new_project_name):
     settings_path = root_path + "/elixir.code-workspace"
     with open(settings_path, encoding="utf-8") as f:
         settings = json.load(f)
@@ -25,13 +25,23 @@ def change_project_dir():
         json.dump(settings, f)
 
 
+def add_commit():
+    os.chdir(root_path)
+    process = subprocess.run("git status -s".split(),stdout=subprocess.PIPE, text=True)
+    for line in process.stdout.split("\n"):
+        if line.startswith("??"):
+            new_dir_name = line[3:-1]
+            subprocess.run("git add -A".split())
+            subprocess.run(f"git commit -m \"{new_dir_name}\"")
+            break
+
+
 def main():
-    execute_and_change_dir()
-    change_project_dir()
-    os.system("history -a")
+    new_project_name = cli_arguments[3].split("=")[1]
+    execute_and_change_dir(new_project_name)
+    change_project_dir(new_project_name)
+    add_commit()
     os.system("/bin/bash")
 
 if __name__ == "__main__":
-    execute_and_change_dir()
-    change_project_dir()
-    os.system("/bin/bash")
+    main()
