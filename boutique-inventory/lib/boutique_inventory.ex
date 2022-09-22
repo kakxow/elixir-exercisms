a9e7d523b626fea8898190bda28375aa2cfb374d
kakxow <kakxow@gmail.com>
2022-09-22 13:06:45 +0300

    "boutique-inventory"

---
+++ boutique-inventory/lib/boutique_inventory.ex
@@ -0,0 +1,28 @@
+defmodule BoutiqueInventory do
+  def sort_by_price(inventory) do
+    Enum.sort_by(inventory, & &1.price)
+  end
+
+  def with_missing_price(inventory) do
+    Enum.filter(inventory, &(&1.price == nil))
+  end
+
+  def update_names(inventory, old_word, new_word) do
+    Enum.map(
+      inventory,
+      &Map.update!(&1, :name, fn name -> String.replace(name, old_word, new_word) end)
+    )
+  end
+
+  def increase_quantity(item, count) do
+    Map.update!(
+      item,
+      :quantity_by_size,
+      &Map.new(&1, fn {k, v} -> {k, v + count} end)
+    )
+  end
+
+  def total_quantity(item) do
+    Enum.reduce(item.quantity_by_size, 0, fn {_k, v}, acc -> acc + v end)
+  end
+end
